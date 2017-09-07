cb49d879e6c5d678efe021d2118e91def67f2189
Austin Beydler <ajb6f4@mail.missouri.edu>
2017-09-06 20:47:42 -0500

    First Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Austin Beydler on 9/6/17.
+//  Copyright © 2017 Austin Beydler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
